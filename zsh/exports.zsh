b73d053447ed8106054e643083efc56f591f83c3
Jessica Lynn Suttles <jlsuttles@gmail.com>
2013-07-29 18:05:46 -0700

    don't always require rubygems, this is sooo old

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -6,11 +6,9 @@ export LSCOLORS=gxfxcxdxbxegedabagacad
 export TERM=xterm-256color
 
 export GREP_COLOR='3;33'
-export GREP_OPTIONS='--color=auto' 
+export GREP_OPTIONS='--color=auto'
 
 export EDITOR='mvim -f -c "au VimLeave * !open -a Terminal"' # edit git commit messages (among other things) in vim
-
-export RUBYOPT='rubygems' # ruby always requires rubygems
 
 # add the MySQL library folder
 # export DYLD_LIBRARY_PATH="/usr/local/Cellar/mysql/5.5.25a/lib"
